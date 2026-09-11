0d05d6d566838a1e371bf85b749d720b433651d3
Ahmed_Taha <154711462+ahmedtahv1@users.noreply.github.com>
2026-09-11 05:03:49 +0300

    Q1,Q2

--- ADVC#01/Program.cs
+++ ADVC#01/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace ADVC_01
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ADVC_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Q1
+            //class can have more than one type ,to code without duplicating the code for the same logic but different data type
+            
+            
         }
+        #region Q2
+        //public class Container<T>
+        //{
+        //    private List<T> _items = new List<T>();
+        //    public void Add(T item)
+        //    {
+        //        _items.Add(item);
+        //    }
+        //    public T Get(int index)
+        //    {
+        //        return _items[index];
+        //    }
+
+        //} 
+        #endregion
+
     }
 }
