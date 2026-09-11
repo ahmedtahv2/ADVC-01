98ec4eca2fd9953df39160f86afcf588f529cd7c
Ahmed_Taha <154711462+ahmedtahv1@users.noreply.github.com>
2026-09-11 05:09:41 +0300

    Q3

--- ADVC#01/Program.cs
+++ ADVC#01/Program.cs
@@ -27,5 +27,22 @@ namespace ADVC_01
         //} 
         #endregion
 
+        #region Q3
+        ////taking more than one type genric for class or method 
+        //public class Pair<TKey,TValue>
+        //{
+        //    public TKey Key { get; set; }
+        //    public TValue Value{ get; set; }
+
+        //    public Pair(TKey key, TValue value)
+        //    {
+        //        Key = key;
+        //        Value = value;
+        //    }
+        //} 
+        #endregion
+
+
+
     }
 }
